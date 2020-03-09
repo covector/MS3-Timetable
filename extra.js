419fd0e7316972d889d152c4caca4906a2117a23
covector <60296643+covector@users.noreply.github.com>
2020-03-09 13:04:14 +0800

    forgot to remove debug alert

--- extra.js
+++ extra.js
@@ -78,7 +78,6 @@ addLesson = function(){
 }
 
 displayExtra = function(cookie){
-    alert(cookie);
     let subject = cookie.substring(19);
     let time = cookie.substring(0, 13);
     let before = document.getElementsByClassName("EmptyExtra")[0];
